e5b33b20dbd63174c5df53800247cb013f5a0a7a
ekta2306 <ektabij@gmail.com>
2024-09-14 22:40:11 +0530

    updated script

--- db.py
+++ db.py
@@ -3,12 +3,10 @@ from pymongo import MongoClient
 import urllib.parse
 
 def get_mongo_client():
-    # Replace these with your actual username, password, and cluster connection string
-    username = urllib.parse.quote_plus("FaultInOurStars")
-    password = urllib.parse.quote_plus("FaultInOurStars")
-    #cluster_url = "cluster0.mongodb.net"  # Your MongoDB cluster URL
+    # For another mongoDB databse, replace with respective credentials
+    username = urllib.parse.quote_plus("trial-user")
+    password = urllib.parse.quote_plus("trial-user")
 
-    # Replace 'myFirstDatabase' with the name of your database
     connection_string = f"mongodb+srv://{username}:{password}@cluster0.tm97tgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
     # Connect to MongoDB Atlas
@@ -18,7 +16,4 @@ def get_mongo_client():
 def get_database():
     # Access the database and collection
     client=get_mongo_client()
-    # Access the database and collection
-    users_collection = client.users
-    articles_collection = client.articles
-    return client['DocumentsTrial']  # Replace with your database name
+    return client['DocumentsTrial']  
